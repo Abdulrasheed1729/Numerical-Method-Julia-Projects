cf7c13b5e95a4c5538b66228792e977455ce9df5
Abdulrasheed Fawole <70612165+Abdulrasheed1729@users.noreply.github.com>
2021-06-04 17:54:48 +0100

    Update rungeKutta.jl

--- rungeKutta.jl
+++ rungeKutta.jl
@@ -3,18 +3,20 @@
    ***********************************
  =#
 
+#f(x,y) = 
+
 function rungeKutta(α::Real, a::Real, h::Real, n::Int64)
     #h = (b-a)/n
-     t = a
-     w = α
+     x = a
+     y = α
         
          for i in 1:n
-           k1 = h * j(t, w)
-           k2 = h * j(t + h/2, w + k1/2)
-           k3 = h * j(t + h/2, w + k2/2)
-           k4 = h * j(t + h,w + k3)
-           w = w + (k1 + 2*(k2 + k3)+ k4)/6
-           t = a + i * h
-            println("t = $t <-----> w = $w")
+           k1 = h * f(x, y)
+           k2 = h * f(x + h/2, y + k1/2)
+           k3 = h * f(x + h/2, y + k2/2)
+           k4 = h * f(x + h,y + k3)
+           y = y + (k1 + 2*(k2 + k3)+ k4)/6
+           x = a + i * h
+            println("x = $x <-----> y = $y")
         end
     end
